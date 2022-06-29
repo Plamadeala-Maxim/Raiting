b928777e00343178fa4d5f3bda52a41f427942f9
Maxim <maximplamadeala13@gmail.com>
2022-06-29 17:06:03 +0300

    primul commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// const toateCinci = document.querySelectorAll('.steluta')
+// const firstS = document.getElementById("stl1")
+// const secondS = document.getElementById("stl2")
+// const thirdS = document.getElementById("stl3")
+// const fourthS = document.getElementById("stl4")
+// const fifthS = document.getElementById("stl5")
+//
+//
+// console.log(toateCinci)
+// // console.log(toateCinci[0])
+// let zero = 0
+//
+// toateCinci.forEach(item =>{
+//     item.addEventListener("click",ev =>{
+//         item.classList.toggle("stelutaGalben")
+//         item.classList.toggle("steluta")
+//         toateCinci.forEach(item =>{
+//             if (item.classList !== "stelutaGalben" && zero === 0){
+//                 item.classList.toggle("stelutaGalben")
+//                 item.classList.toggle("steluta")
+//             }else{
+//                 zero = 1
+//             }
+//         })
+//
+//     })
+//
+// })
+
+
+
+
+
+
+
+// const da = (arr) => {
+//     if (arr[])
+// }
