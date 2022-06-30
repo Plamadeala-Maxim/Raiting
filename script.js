0b3cfefed3fd90b8f74d45894c8bdb2b292af3df
Maxim <maximplamadeala13@gmail.com>
2022-06-30 15:19:07 +0300

    am finisat Pr

--- script.js
+++ script.js
@@ -6,33 +6,43 @@
 // const fifthS = document.getElementById("stl5")
 //
 //
-// console.log(toateCinci)
+// // console.log(toateCinci)
 // // console.log(toateCinci[0])
-// let zero = 0
+// // let zero = 0
 //
-// toateCinci.forEach(item =>{
-//     item.addEventListener("click",ev =>{
+// toateCinci.forEach((item, index) => {
+//     // console.log(index)
+//     item.addEventListener("click", ev => {
 //         item.classList.toggle("stelutaGalben")
 //         item.classList.toggle("steluta")
-//         toateCinci.forEach(item =>{
-//             if (item.classList !== "stelutaGalben" && zero === 0){
-//                 item.classList.toggle("stelutaGalben")
-//                 item.classList.toggle("steluta")
-//             }else{
-//                 zero = 1
-//             }
-//         })
+//         console.log(index)
+//         if (index === 3 && toateCinci[4].classList !=="stelutaGalben") {
+//             toateCinci[4].classList.toggle("stelutaGalben")
+//             toateCinci[4].classList.toggle("steluta")
+//         } else if (index === 2){
+//             toateCinci[3].classList.toggle("stelutaGalben")
+//             toateCinci[3].classList.toggle("steluta")
+//         }else if (index === 1){
+//             toateCinci[2].classList.toggle("stelutaGalben")
+//             toateCinci[2].classList.toggle("steluta")
+//         }
+//             })
+// item.addEventListener("click", e =>{
 //
-//     })
-//
+// })
+
 // })
 
 
 
+// toateCinci.forEach(item =>{
+//     if (item.classList !== "stelutaGalben" && zero === 0){
+//         item.classList.toggle("stelutaGalben")
+//         item.classList.toggle("steluta")
+//     }else{
+//         zero = 1
+//     }
+// })
 
 
-
-
-// const da = (arr) => {
-//     if (arr[])
-// }
+// const da = (ar
